44c301a51144b8256368e0c18dc384d54e3a3f4b
arfiancahya <arfian.dwisetya@gmail.com>
2020-10-25 09:07:02 +0700

    Menambahkan Sass Login

--- mern_client/src/pages/login/index.js
+++ mern_client/src/pages/login/index.js
@@ -1,11 +1,18 @@
 import React,{ Fragment }  from 'react';
+import Gambar from '../../asset/images/amico.svg'
+import './login.scss'
 
 const Login = () => {
     return (
         <Fragment>
-            <div>
-                <h1>Login</h1>
+        <div class="main-page">
+            <cdiv class="left">
+                <img src={Gambar} alt="gambar" />
+            </cdiv>
+            <div class="right">
+                <h1>Admin Login</h1>
             </div>
+        </div>
         </Fragment>
     );
 }
