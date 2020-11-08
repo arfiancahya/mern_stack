f5a0e91f8f1bc1a511d20f121db9720f0e51be0b
arfiancahya <arfian.dwisetya@gmail.com>
2020-11-08 14:31:28 +0700

    Membuat icon

--- mern_client/src/pages/login/index.js
+++ mern_client/src/pages/login/index.js
@@ -1,18 +1,22 @@
-import React,{ Fragment }  from 'react';
-import Gambar from '../../asset/images/amico.svg'
+import React, { Fragment } from 'react';
 import './login.scss'
 
 const Login = () => {
     return (
         <Fragment>
-        <div class="main-page">
-            <cdiv class="left">
-                <img src={Gambar} alt="gambar" />
-            </cdiv>
-            <div class="right">
-                <h1>Admin Login</h1>
+            <div class="main-page">
+                <div class="left">
+                    <h1>Sign in to Blogture</h1>
+                    <div className="icon-sm">
+                        <i className="fab fa-facebook-f fa-lg"></i>
+                        <i className="fab fa-google-plus-g fa-lg"></i>
+                        <i className="fab fa-linkedin-in fa-lg"></i>
+                    </div>
+                </div>
+                <div class="right">
+                    <h1>Admin Login</h1>
+                </div>
             </div>
-        </div>
         </Fragment>
     );
 }
