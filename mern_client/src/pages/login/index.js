cfeed0032154027ded9a1dabd3a7180495afdc5f
arfiancahya <arfian.dwisetya@gmail.com>
2020-11-09 13:02:32 +0700

    Menambahkan Atomic Scss

--- mern_client/src/pages/login/index.js
+++ mern_client/src/pages/login/index.js
@@ -1,19 +1,19 @@
 import React, { Fragment } from 'react';
-import './login.scss'
+import '../pages.scss'
 
 const Login = () => {
     return (
         <Fragment>
-            <div class="main-page">
-                <div class="left">
-                    <h1>Sign in to Blogture</h1>
-                    <div className="icon-sm">
+            <div className="flex-normal height-100">
+                <div className="flex-normal flex-1 flex-direction-coloumn flex-content-center text-align-center">
+                    <h1 className="font-color-green">Sign in to Blogture</h1>
+                    <div className="text-align-center">
                         <i className="fab fa-facebook-f fa-lg"></i>
                         <i className="fab fa-google-plus-g fa-lg"></i>
                         <i className="fab fa-linkedin-in fa-lg"></i>
                     </div>
                 </div>
-                <div class="right">
+                <div className="width-30 text-align-center background-color-red flex-normal flex-direction-coloumn flex-content-center text-align-center">
                     <h1>Admin Login</h1>
                 </div>
             </div>
