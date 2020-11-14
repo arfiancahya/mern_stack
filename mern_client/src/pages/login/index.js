75076ffb6503e1e665a8b10f040dcbcd4a130042
arfiancahya <arfian.dwisetya@gmail.com>
2020-11-14 17:26:57 +0700

    Memperbaiki style login

--- mern_client/src/pages/login/index.js
+++ mern_client/src/pages/login/index.js
@@ -8,7 +8,7 @@ const Login = () => {
             <div className="flex-normal height-100">
                 <div className="flex-normal flex-1 flex-direction-coloumn flex-content-center text-align-center">
                     <h1 className="font-color-green font-size-45 font-bold">Sign in to Blogture</h1>
-                    <Gap height={50}/>
+                    <Gap height={60}/>
                     <div className="text-align-center">
                         <i className="fab fa-facebook-f fa-lg"/>
                         <i className="fab fa-google-plus-g fa-lg margin-right-20 margin-left-20" />
@@ -16,6 +16,7 @@ const Login = () => {
                     </div>
                     <Gap height={40}/>
                     <p className="font-color-gray-89">or use your email account</p>
+                    <Gap height={20}/>
                     <div className="login flex-normal flex-content-center">
                         <i className="far fa-envelope fa-lg min-width-50  background-color-grey padding-top-20 padding-left-18 padding-btm-13 border-top-left-10 border-btm-left-10" />
                         <Input className="width-350 padding-20 border-top-right-10 border-btm-right-10 outline-none box-sizing border-none background-color-grey" placeholder="Email" />
@@ -27,12 +28,15 @@ const Login = () => {
                     </div>
                     <Gap height={30}/>
                     <p className="font-color-black font-bold">Forgot your password</p>
-                    <Gap height={20}/>
+                    <Gap height={40}/>
                     <Button title="Sign In" className="background-color-green width-74 padding-18 font-upper font-color-white border-none border-radius-24 box-sizing outline-none" />
                 </div>
-                <div className="width-30 text-align-center background-color-red flex-normal flex-direction-coloumn flex-content-center text-align-center">
-                    <h1>Hello Friend</h1>
-                    <p>Enter your personal details and start journey with us </p>
+                <div className="width-28 text-align-center background-color-gradient flex-normal flex-direction-coloumn flex-content-center text-align-center background-image background-cover">
+                    <h1 className="font-size-50 font-color-white font-bold">Hello, Friend!</h1>
+                    <Gap height={50}/>
+                    <p className="font-color-white margin-left-40 margin-right-40">Enter your personal details and start journey with us </p>
+                    <Gap height={50}/>
+                    <Button title="Sign In" className="background-color-trans width-74 padding-12 font-upper font-color-white border-radius-24 box-sizing outline-none" />
                 </div>
             </div>
         </Fragment>
